27e4fde83594e1783c8255a05a463a7e83cf503e
avishaihalev <avishai.halev@external.total.com>
2022-12-02 15:07:35 -0800

    cast action space dtype as well

--- envs/gym_env.py
+++ envs/gym_env.py
@@ -4,7 +4,8 @@ from garage.envs import GymEnv as _GarageGymEnv
 class GymEnv(_GarageGymEnv):
     def __init__(self, env, is_image=False, max_episode_length=None):
         super().__init__(env, is_image=is_image, max_episode_length=max_episode_length)
-        self._coerce_dtypes()
+        self._cast_dtypes()
 
-    def _coerce_dtypes(self):
+    def _cast_dtypes(self):
         self._observation_space.dtype = self._env.observation_space.dtype
+        self._action_space.dtype = self._env.action_space.dtype
