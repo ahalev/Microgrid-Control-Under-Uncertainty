7b5fe761887c2f1af298b641ef36dc95d7d72680
avishaihalev <avishai.halev@external.total.com>
2023-01-02 12:17:32 -0800

    add update to namespacify

--- config/namespacify.py
+++ config/namespacify.py
@@ -10,7 +10,28 @@ class Namespacify(UserDict):
                 in_dict[key] = Namespacify(key, in_dict[key])
 
         super().__init__(in_dict)
-        self.__dict__.update(in_dict)
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) != 1 or not isinstance(args[0], (dict, UserDict)):
+                raise TypeError('Invalid arguments')
+            elif kwargs:
+                raise TypeError('Cannot pass both args and kwargs.')
+
+            d = args[0]
+        else:
+            d = kwargs
+
+        for k, v in d.items():
+            if isinstance(v, (dict, UserDict)):
+                if k in self:
+                    self[k].update(v)
+                else:
+                    self[k] = Namespacify(k, v)
+            else:
+                super().update({k: v})
+
+        return self
 
     def pprint(self, indent=0):
         print("{}{}:".format(' ' * indent, self.name))
