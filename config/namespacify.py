159b689edaa70185f0b9851187fab3c2c990e538
avishaihalev <avishai.halev@external.total.com>
2023-01-18 23:26:38 -0800

    add nested_dict_update function

--- config/namespacify.py
+++ config/namespacify.py
@@ -12,26 +12,7 @@ class Namespacify(UserDict):
         super().__init__(in_dict)
 
     def update(self, *args, **kwargs):
-        if args:
-            if len(args) != 1 or not isinstance(args[0], (dict, UserDict)):
-                raise TypeError('Invalid arguments')
-            elif kwargs:
-                raise TypeError('Cannot pass both args and kwargs.')
-
-            d = args[0]
-        else:
-            d = kwargs
-
-        for k, v in d.items():
-            if isinstance(v, (dict, UserDict)):
-                if k in self:
-                    self[k].update(v)
-                else:
-                    self[k] = Namespacify(k, v)
-            else:
-                super().update({k: v})
-
-        return self
+        return nested_dict_update(self, *args, nest_namespacify=True, **kwargs)
 
     def pprint(self, indent=0):
         print("{}{}:".format(' ' * indent, self.name))
@@ -56,3 +37,38 @@ class Namespacify(UserDict):
             return self[item]
         except KeyError:
             raise AttributeError(item)
+
+    def __xor__(self, other):
+        diff = {}
+        for k, v in self.items():
+            assert k in other
+            if v != other[k]:
+                if isinstance(v, Namespacify):
+                    diff[k] = v.__xor__(other[k])
+                else:
+                    diff[k] = v
+
+        return Namespacify(self.name, diff)
+
+
+def nested_dict_update(nested_dict, *args, nest_namespacify=False, **kwargs):
+    if args:
+        if len(args) != 1 or not isinstance(args[0], (dict, UserDict)):
+            raise TypeError('Invalid arguments')
+        elif kwargs:
+            raise TypeError('Cannot pass both args and kwargs.')
+
+        d = args[0]
+    else:
+        d = kwargs
+
+    for k, v in d.items():
+        if isinstance(v, (dict, UserDict)):
+            if k in nested_dict:
+                nested_dict[k].update(v)
+            else:
+                nested_dict[k] = Namespacify(k, v) if nest_namespacify else v
+        else:
+            nested_dict[k] = v
+
+    return nested_dict
