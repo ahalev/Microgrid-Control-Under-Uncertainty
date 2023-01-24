78d9ff7f59fc4ebe7a8cac4593ba43cddd33ac9b
avishaihalev <avishai.halev@external.total.com>
2023-01-24 15:10:17 -0800

    add sequential log dir

---
+++ logger/log_dir.py
@@ -0,0 +1,28 @@
+import os
+
+
+def _make_sequential_log_dir(log_dir):
+    """Creates log_dir, appending a number if necessary.
+
+    Attempts to create the directory `log_dir`. If it already exists, appends
+    "_1". If that already exists, appends "_2" instead, etc.
+
+    Args:
+        log_dir (str): The log directory to attempt to create.
+
+    Returns:
+        str: The log directory actually created.
+
+    """
+    i = 0
+    while True:
+        try:
+            if i == 0:
+                os.makedirs(log_dir)
+            else:
+                possible_log_dir = '{}_{}'.format(log_dir, i)
+                os.makedirs(possible_log_dir)
+                log_dir = possible_log_dir
+            return log_dir
+        except FileExistsError:
+            i += 1
