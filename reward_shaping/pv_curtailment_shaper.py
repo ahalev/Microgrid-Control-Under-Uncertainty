ff8c40a5e76dc1c52105309d64e800b1a640b416
avishaihalev <avishai.halev@external.total.com>
2023-02-08 08:46:05 -0800

    update pv_curtailment_shaper for sum_module_val method

--- reward_shaping/pv_curtailment_shaper.py
+++ reward_shaping/pv_curtailment_shaper.py
@@ -5,10 +5,5 @@ class PVCurtailmentShaper(BaseRewardShaper):
     yaml_tag = u"!PVCurtailmentShaper"
 
     def __call__(self, step_info, cost_info):
-        try:
-            pv_info = step_info['pv']
-        except KeyError:
-            return 0.0
-        else:
-            curtailment = [d['curtailment'] for d in pv_info]
-            return -1.0 * sum(curtailment)
+        pv_curtailment = self.sum_module_val(step_info, 'pv', 'curtailment')
+        return -1.0 * sum(pv_curtailment)
