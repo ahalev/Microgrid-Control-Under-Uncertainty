36f6884ebb5740185d02f604e925c7e30680999f
avishaihalev <avishai.halev@external.total.com>
2023-03-10 12:26:07 -0800

    update funcs

--- analysis/result_loader.py
+++ analysis/result_loader.py
@@ -63,22 +63,82 @@ class ResultLoader(Namespacify):
 
         return locations
 
-    def plot_reward_cumsum(self):
+    def _save_file(self, suffix):
+        if self.save_dir is not None:
+            file = self.save_dir / suffix
+
+            if not file.parent.exists():
+                file.parent.mkdir(parents=True)
+
+            print(f'Saving to file:\n\t{file.absolute()}')
+            return file
+
+        return None
+
+    def plot_reward_cumsum(self, relative_to=None, hue=None, style=None, relplot_col=None):
         rewards_dict = {}
         for eval_log in self.evaluate_logs:
             log = self[eval_log].log
-            rewards_dict[eval_log[1]] = log[('balance', '0', 'reward')].cumsum()
+            rewards_dict[eval_log[:-1]] = log[('balance', '0', 'reward')].cumsum()
+
+        cost_column_name = f'{"Relative "*(relative_to is not None)}Cumulative Cost'
 
         rewards = -1 * pd.DataFrame(rewards_dict)
-        rewards /= rewards['modelpredictivecontrol'].values.reshape(-1, 1)
-        rewards = rewards.iloc[10:]
-        rewards = rewards.unstack().reset_index(name='Cumulative Cost')
-        rewards = rewards.rename(columns={'level_0': 'Experiment', 'level_1': 'Step'})
+        if relative_to is not None:
+            relative_to_loc = [relative_to in level for level in rewards.columns.levels]
+            idx = tuple(slice(None) if not loc else relative_to for loc in relative_to_loc)
+            values_relative_to = rewards.loc[:, idx]
+            values_relative_to = values_relative_to.droplevel(np.where(relative_to_loc)[0].item(), axis=1)
 
-        g = sns.lineplot(data=rewards, x='Step', y='Cumulative Cost', hue='Experiment')
+            for col in values_relative_to.columns:
+                rewards[col] = rewards[col].div(values_relative_to[col], axis=0)
+
+        param_cols = [f'level_{j}' for j in range(len(self.evaluate_logs[0][:-1]))]
+
+        rewards = rewards.iloc[10:]
+        rewards = rewards.unstack().reset_index(name=cost_column_name)
+
+        rewards = pd.concat([rewards.drop(columns=param_cols), self._extract_param_columns(rewards[param_cols])], axis=1)
+
+        rewards = rewards.rename(columns={f'level_{len(self.evaluate_logs[0])-1}': 'Step'})
+
+        min_col_wrap, max_col_wrap = 2, 5
+
+        if relplot_col is not None:
+            nunique_col = rewards[relplot_col].nunique()
+            col_wrap = nunique_col / np.arange(min_col_wrap, max_col_wrap+1)
+            col_wrap = np.where(col_wrap > 2)[0].max() + min_col_wrap
+        else:
+            col_wrap = int((min_col_wrap+max_col_wrap) // 2)
+
+        g = sns.relplot(
+            data=rewards,
+            x='Step',
+            y=cost_column_name,
+            kind='line',
+            hue=hue,
+            style=style,
+            col=relplot_col,
+            col_wrap=col_wrap,
+            palette=sns.color_palette("rocket_r", n_colors=rewards[hue].nunique()))
+
+        if relative_to is not None:
+            g.set(ylim=(0.5, 1.5))
+
+        save_file = self._save_file('reward_cumsum.png')
+        if save_file:
+            plt.savefig(save_file)
+
         plt.show()
 
-        print('here')
+    def _extract_param_columns(self, df):
+        new_df = {}
+        for col in df.columns:
+            split = df[col].str.split('_', expand=True)
+            label = split.iloc[:, 0].unique().item().title()
+            new_df[label] = pd.to_numeric(split.iloc[:, 1], errors='ignore')
+
+        return pd.DataFrame(new_df, index=df.index)
 
 
 if __name__ == '__main__':
