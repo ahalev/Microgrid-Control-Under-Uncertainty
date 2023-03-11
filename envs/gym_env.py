59c0873c70c9b736c13d6601dedc6f5f1d8e9017
avishaihalev <avishai.halev@external.total.com>
2023-03-11 14:12:42 -0800

    add GymEnv setattr

--- envs/gym_env.py
+++ envs/gym_env.py
@@ -9,3 +9,19 @@ class GymEnv(_GarageGymEnv):
     def _cast_dtypes(self):
         self._observation_space.dtype = self._env.observation_space.dtype
         self._action_space.dtype = self._env.action_space.dtype
+
+    def __setattr__(self, key, value):
+        # This is a hack, but this is the last thing initialized in super().__init__.
+        # Prevents conflicts in GymEnv attributes (albeit only those in defined in __init__) and anything in 'env'.
+        post_init = hasattr(self, '_env_info')
+
+        try:
+            self.__getattribute__(key)
+            toplevel_attr = True
+        except AttributeError:
+            toplevel_attr = False
+
+        if post_init and not toplevel_attr and hasattr(self._env, key):
+            setattr(self._env, key, value)
+        else:
+            super().__setattr__(key, value)
