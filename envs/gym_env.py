3ff3c63199ac0db85e6ee166f6fd922c15013ab6
avishaihalev <avishai.halev@external.total.com>
2023-04-07 20:58:38 -0700

    update setattr in gymenv

--- envs/gym_env.py
+++ envs/gym_env.py
@@ -3,7 +3,9 @@ from garage.envs import GymEnv as _GarageGymEnv
 
 class GymEnv(_GarageGymEnv):
     def __init__(self, env, is_image=False, max_episode_length=None):
+        self._post_init = False
         super().__init__(env, is_image=is_image, max_episode_length=max_episode_length)
+        self._post_init = True
         self._cast_dtypes()
 
     def _cast_dtypes(self):
@@ -11,17 +13,13 @@ class GymEnv(_GarageGymEnv):
         self._action_space.dtype = self._env.action_space.dtype
 
     def __setattr__(self, key, value):
-        # This is a hack, but this is the last thing initialized in super().__init__.
-        # Prevents conflicts in GymEnv attributes (albeit only those in defined in __init__) and anything in 'env'.
-        post_init = hasattr(self, '_env_info')
-
         try:
             self.__getattribute__(key)
             toplevel_attr = True
         except AttributeError:
             toplevel_attr = False
 
-        if post_init and not toplevel_attr and hasattr(self._env, key):
+        if key != '_post_init' and self._post_init and not toplevel_attr and hasattr(self._env, key):
             setattr(self._env, key, value)
         else:
             super().__setattr__(key, value)
