2ba0f30b464c0484af8a8bae6e860392b0fff20c
avishaihalev <avishai.halev@external.total.com>
2023-05-20 17:17:10 -0700

    bc updates

--- pretrain/bc.py
+++ pretrain/bc.py
@@ -1,8 +1,11 @@
 import torch
+import numpy as np
 
-from garage import _Default
+from garage import _Default, make_optimizer
+from garage.torch import np_to_torch
 from garage.torch.algos import BC as _GarageBC
 
+# TODO need to use this, figure out how to initialize qf (and value function for ppo)
 
 class BC(_GarageBC):
     def __init__(
@@ -18,7 +21,9 @@ class BC(_GarageBC):
             loss='log_prob',
             minibatches_per_epoch=16,
             name='BC',
-            qf=None
+            qf=None,
+            qf_optimizer=torch.optim.Adam,
+            qf_lr=_Default(1e-3)
     ):
         super().__init__(
             env_spec,
@@ -34,3 +39,35 @@ class BC(_GarageBC):
         )
 
         self._qf = qf
+        self._qf_optimizer = make_optimizer(qf_optimizer,
+                                            module=self._qf,
+                                            lr=qf_lr)
+
+    def _train_once(self, trainer, epoch):
+        """Obtain samplers and train for one epoch.
+
+        Args:
+            trainer (Trainer): Experiment trainer, which may be used to
+                obtain samples.
+            epoch (int): The current epoch.
+
+        Returns:
+            List[float]: Losses.
+
+        """
+        if self._qf is None:
+            return super()._train_once(trainer, epoch)
+
+        batch = self._obtain_samples(trainer, epoch)
+        indices = np.random.permutation(len(batch.actions))
+        minibatches = np.array_split(indices, self._minibatches_per_epoch)
+        losses = []
+        for minibatch in minibatches:
+            observations = np_to_torch(batch.observations[minibatch])
+            actions = np_to_torch(batch.actions[minibatch])
+            self._optimizer.zero_grad()
+            loss = self._compute_loss(observations, actions)
+            loss.backward()
+            losses.append(loss.item())
+            self._optimizer.step()
+        return losses
