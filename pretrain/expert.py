6b9eacbd569ff3f08ff72d1637522acec893bc5b
avishaihalev <avishai.halev@external.total.com>
2023-06-08 11:08:46 -0700

    allow multiple workers in expert

--- pretrain/expert.py
+++ pretrain/expert.py
@@ -12,12 +12,13 @@ from pymgrid import algos
 
 
 class Expert(Iterator):
-    def __init__(self, expert_type, episodes_per_batch, additional_config=None):
+    def __init__(self, expert_type, episodes_per_batch, n_workers=1, additional_config=None):
         self.trainer = self._get_trainer(expert_type, additional_config)
         self.episodes_per_batch = episodes_per_batch
         self.env = self.trainer.env
         self.agent = ExpertAgent(algo=self.trainer.algo, env=self.env)
-        self.worker = self._get_worker()
+        self.n_workers = n_workers
+        self.workers = self._get_workers()
         self.stats = None
 
     def _get_trainer(self, expert_type, additional_config):
@@ -33,18 +34,32 @@ class Expert(Iterator):
 
         raise ValueError(f"expert_type must be 'rbc' or 'mpc', not '{expert_type}'.")
 
-    def _get_worker(self):
-        worker = WorkerFactory(
+    def _get_workers(self):
+        factory = WorkerFactory(
             max_episode_length=self.env.spec.max_episode_length,
             seed=get_seed(),
-            n_workers=1
-        )(0)
-        worker.update_env(self.env)
-        worker.update_agent(self.agent)
-        return worker
+            n_workers=self.n_workers
+        )
+
+        workers = [factory(i) for i in range(self.n_workers)]
+
+        for worker in workers:
+            worker.update_env(self.env)
+            worker.update_agent(self.agent)
+
+        return workers
 
     def generate_batch(self):
-        batches = [self.worker.rollout() for _ in range(self.episodes_per_batch)]
+        n_eps = 0
+        batches = []
+
+        while True:
+            for worker in self.workers:
+                batches.append(worker.rollout())
+                n_eps += 1
+            if n_eps >= self.episodes_per_batch:
+                break
+
         samples = EpisodeBatch.concatenate(*batches)
         self._log_rollout(samples)
         return samples
@@ -61,12 +76,12 @@ class Expert(Iterator):
 
     def __getstate__(self):
         state = self.__dict__.copy()
-        state['worker'] = None
+        state['workers'] = None
         return state
 
     def __setstate__(self, state):
         self.__dict__.update(state)
-        self.worker = self._get_worker()
+        self.workers = self._get_workers()
 
     def __iter__(self):
         return self
