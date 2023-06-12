fd86d94620e10a3d4ee2ebb5eb8e974a87db13f8
avishaihalev <avishai.halev@external.total.com>
2023-06-11 22:48:57 -0700

    finish rnd model

--- rnd/rnd_trainer.py
+++ rnd/rnd_trainer.py
@@ -1,6 +1,14 @@
 import torch
+import treetensor.numpy as tnp
+import treetensor.torch as ttorch
 
+from copy import deepcopy
+
+from ding.data import DequeBuffer
+from ding.framework.middleware import offpolicy_data_fetcher
 from ding.reward_model import RndRewardModel
+
+
 from garage import Trainer
 
 
@@ -8,7 +16,10 @@ class RNDTrainer(Trainer):
     def __init__(self, snapshot_config, rnd_config, obs_space):
         self._step_itr = None
         super().__init__(snapshot_config)
-        self._reward_model = self._setup_rnd_model(rnd_config, obs_space)
+
+        self._reward_model = self._setup_rnd_model(rnd_config.model, obs_space)
+        self._rnd_buffer = DequeBuffer(size=rnd_config.buffer.size)
+        self._rnd_data_fetcher = self._setup_rnd_data_fetcher(rnd_config)
         self._current_episodes = None
 
     def _setup_rnd_model(self, rnd_config, obs_space):
@@ -22,22 +33,40 @@ class RNDTrainer(Trainer):
 
         return RndRewardModel(rnd_config, device)
 
+    def _setup_rnd_data_fetcher(self, config):
+        return offpolicy_data_fetcher(config, self._rnd_buffer)
+
     def obtain_episodes(self,
                         itr,
                         batch_size=None,
                         agent_update=None,
                         env_update=None):
-        # TODO append intrinsic rewards
         episodes = super().obtain_episodes(itr, batch_size=batch_size, agent_update=agent_update, env_update=env_update)
-        self._current_episodes = episodes.copy()
+        self._current_episodes = deepcopy(episodes)
         episodes = self._append_intrinsic_rewards(episodes)
         return episodes
 
     def _append_intrinsic_rewards(self, episodes):
-        pass
+        # TODO need to add to padded rewards too
+        # TODO append intrinsic rewards
+        # TODO need to make sure the extrinsic rewards are somehow normalized here
+        # TODO tensorboard writer
+
+        timesteps = [
+            ttorch.tensor({'obs': obs, 'reward': reward, 'done': done}).float()
+            for obs, reward, done in zip(episodes.observations, episodes.rewards, episodes.terminals)
+        ]
+
+        self._reward_model.collect_data(timesteps)
+        augmented_rewards = self._reward_model.estimate(timesteps)
+        episodes.rewards[:] = [x['reward'].item() for x in augmented_rewards]
+
+        return episodes
 
     def _train_rnd_model(self):
-        pass
+        self._reward_model.train()
+        self._reward_model.clear_data()
+        return None
 
     @property
     def step_itr(self):
