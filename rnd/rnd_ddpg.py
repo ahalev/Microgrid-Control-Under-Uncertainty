a4d4fe799011f62513d0374ae627dec8ab9bbd55
avishaihalev <avishai.halev@external.total.com>
2023-06-13 00:58:52 -0700

    change rnd model to mixin

--- rnd/rnd_ddpg.py
+++ rnd/rnd_ddpg.py
@@ -7,60 +7,9 @@ from garage import _Default
 from garage.torch.algos import DDPG
 
 
-class RNDDDPG(DDPG):
-    def __init__(
-            self,
-            env_spec,
-            policy,
-            qf,
-            replay_buffer,
-            sampler,
-            rnd_model,
-            *,
-            steps_per_epoch=20,
-            n_train_steps=50,
-            max_episode_length_eval=None,
-            buffer_batch_size=64,
-            min_buffer_size=int(1e4),
-            exploration_policy=None,
-            target_update_tau=0.01,
-            discount=0.99,
-            policy_weight_decay=0,
-            qf_weight_decay=0,
-            policy_optimizer=torch.optim.Adam,
-            qf_optimizer=torch.optim.Adam,
-            policy_lr=_Default(1e-4),
-            qf_lr=_Default(1e-3),
-            clip_pos_returns=False,
-            clip_return=np.inf,
-            max_action=None,
-            reward_scale=1.
-    ):
-        super().__init__(
-            env_spec,
-            policy,
-            qf,
-            replay_buffer,
-            sampler,
-            steps_per_epoch=steps_per_epoch,
-            n_train_steps=n_train_steps,
-            max_episode_length_eval=max_episode_length_eval,
-            buffer_batch_size=buffer_batch_size,
-            min_buffer_size=min_buffer_size,
-            exploration_policy=exploration_policy,
-            target_update_tau=target_update_tau,
-            discount=discount,
-            policy_weight_decay=policy_weight_decay,
-            qf_weight_decay=qf_weight_decay,
-            policy_optimizer=policy_optimizer,
-            qf_optimizer=qf_optimizer,
-            policy_lr=policy_lr,
-            qf_lr=qf_lr,
-            clip_pos_returns=clip_pos_returns,
-            clip_return=clip_return,
-            max_action=max_action,
-            reward_scale=reward_scale
-        )
+class RNDMixin:
+    def __init__(self, rnd_model, **kwargs):
+        super().__init__(**kwargs)
         self._rnd_model = rnd_model
 
     def train_once(self, itr, episodes):
